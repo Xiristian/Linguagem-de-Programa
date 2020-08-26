746006d11b23422b6b529ecd27d310d35689b3bd
Xiristian <christianhederson22@gmail.com>
2020-08-26 16:47:40 -0300

    :rocket: Operadores em Python

---
+++ aula4.py
@@ -0,0 +1,43 @@
+numeroA = 2
+numeroB = 3
+
+# Operadores aritiméticos
+
+# Soma
+respostaSoma = numeroA + numeroB
+print("resposta da soma:", respostaSoma)
+print("resposta da soma:", (numeroA + numeroB))
+
+# Subtração
+respostaSubtracao = numeroA - numeroB
+print("resposta da subtração:", respostaSubtracao)
+print("resposta da subtração:", (numeroA - numeroB))
+
+# Multiplicação
+respostaMultiplicação = numeroA * numeroB
+print("resposta da multiplicação:", respostaMultiplicação)
+print("resposta da multiplicação:", (numeroA * numeroB))
+
+# Divisão
+respostaDivisao = numeroA / numeroB
+print("resposta da divisão:", respostaDivisao)
+print("resposta da divisão:", (numeroA / numeroB))
+
+# Expoente
+respostaExpoente = numeroA ** numeroB
+print("resposta da exponencial:", respostaExpoente)
+print("resposta da exponencial:", (numeroA ** numeroB))
+
+# Operadores Lógicos e Relacionais
+print(numeroA < 5 and numeroB < 10)
+print(numeroA < 5 or numeroB > 20)
+print(not (numeroA < 5 or numeroB > 20))
+print(numeroA != numeroB)
+
+# Usando input para ler do teclado um dado
+ # String
+armazenaValorTecladoString = input("Digite um Texto:")
+print("O texto digitado foi:", armazenaValorTecladoString)
+ # Inteiro
+armazenaValorTecladoInt = int(input("Digite um número inteiro:"))
+print("O valor inteiro é:", armazenaValorTecladoInt)
